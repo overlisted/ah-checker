412b078f0e5819f3c7129bb893e99b0ee83a19d7
overlisted <mail@overlisted.net>
2020-01-17 22:35:07 +0100

    починил отключающийся лонг полл

--- src/longpoll.js
+++ src/longpoll.js
@@ -7,17 +7,24 @@ async function longPollWorker(response) {
   })
 }
 
+async function getNewLongPollServer() {
+  return (await util.requireVKAPI("groups.getLongPollServer", `group_id=${util.groupID}`)).response;
+}
+
 async function runLongPoll() {
-  const longPoll = (await util.requireVKAPI("groups.getLongPollServer", `group_id=${util.groupID}`)).response;
+  let longPoll = await getNewLongPollServer();
 
   while(true) {
     const response = await util.fetchJSON(
       `${longPoll.server}?act=a_check&key=${longPoll.key}&wait=25&mode=2&ts=${longPoll.ts}`
     );
 
-    if(response.updates) await longPollWorker(response);
-
-    longPoll.ts = response.ts;
+    if(response.updates) {
+      await longPollWorker(response);
+      longPoll.ts = response.ts;
+    } else if(response.failed === 2 || response.failed === 3) {
+      longPoll = await getNewLongPollServer();
+    }
   }
 }
 
